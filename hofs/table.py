7302e23352250c4f36fab624b8a1425ad8995746
uhasker <uhasker@protonmail.com>
2022-07-12 18:54:29 +0200

    feat: improve Table

--- hofs/table.py
+++ hofs/table.py
@@ -1,56 +1,180 @@
 import json
-from typing import List, Dict, Any
+from typing import List, Dict, Union
 
 from hofs import HofsException
 
 
 class Table:
     def __init__(self, cols: List[str]) -> None:
-        self.columns: Dict[str, List] = {col: [] for col in cols}
+        if len(cols) == 0:
+            raise HofsException("table must have at least one column")
 
-    def add_row(self, row: Dict[str, str]) -> None:
-        cols_diff = set(row.keys()).symmetric_difference(set(self.columns.keys()))
-        if len(cols_diff) != 0:
-            raise HofsException(cols_diff)
+        self._cols: Dict[str, List[str]] = {col: [] for col in cols}
 
-        for k, v in row.items():
-            self.columns[k].append(v)
+    def col_name(self, idx: int) -> str:
+        """
+        Get the name of a column from its index.
 
-    def __getitem__(self, item: int) -> Dict[str, Any]:
-        return {k: self.columns[k][item] for k in self.columns.keys()}
+        :param idx: The column index.
+        :return: The column name.
+        """
+        return self.col_names[idx]
 
-    def __len__(self) -> int:
-        key = list(self.columns.keys())[0]
-        return len(self.columns[key])
+    @property
+    def col_names(self) -> List[str]:
+        """
+        The names of the table columns.
+        """
+        return list(self._cols.keys())
 
-    def __str__(self) -> str:
-        ljust_values = {}
-        for col_name, col in self.columns.items():
-            ljust_values[col_name] = (
-                max([len(str(v)) for v in col] + [len(col_name)]) + 1
+    def col(self, idx: int) -> List[str]:
+        """
+        Get a column by index.
+
+        This is equivalent to col_by_name(col_name(idx)).
+
+        :param idx: The column index.
+        :return: A list containing the values of the column.
+        """
+        return self.col_by_name(self.col_name(idx))
+
+    def col_by_name(self, name: str) -> List[str]:
+        """
+        Get a column by name.
+
+        :param name: The column name.
+        :return: A list containing the values of the column.
+        """
+        return self._cols[name]
+
+    def value(self, row_idx: int, col_idx: int) -> str:
+        """
+        Get a value from the table.
+
+        :param row_idx: The index of the row.
+        :param col_idx: The index of the column.
+        :return: The value residing at row with index row_idx and column with index col_index.
+        """
+        return self.col(col_idx)[row_idx]
+
+    def value_by_name(self, row_idx: int, col_name: str) -> str:
+        """
+        Get a value from the table.
+
+        :param row_idx: The index of the row.
+        :param col_name: The name of the column.
+        :return: The value residing at row with index row_idx and column with name col_name.
+        """
+        return self.col_by_name(col_name)[row_idx]
+
+    def row(self, idx: int) -> List[str]:
+        """
+        Get a row.
+
+        :param idx: The row index.
+        :return: A list containing the row values.
+        """
+        return list(self.row_dict(idx).values())
+
+    def row_dict(self, idx: int) -> Dict[str, str]:
+        """
+        Get a row.
+
+        :param idx: The row index.
+        :return: A dictionary containing the columns along with their values for the respective row.
+        """
+        return {
+            col_name: self.value_by_name(idx, col_name) for col_name in self.col_names
+        }
+
+    @property
+    def n_rows(self) -> int:
+        """
+        The number of rows.
+        """
+        assert self.n_cols != 0
+
+        first_col = self.col(0)
+        return len(first_col)
+
+    @property
+    def n_cols(self) -> int:
+        """
+        The number of columns.
+        """
+        return len(self.col_names)
+
+    def add_row(self, row: Union[Dict[str, str], List[str]]) -> None:
+        """
+        Add a row to the table.
+
+        :param row: The row. It must be either a list of values
+        :return:
+        """
+        if isinstance(row, List):
+            if len(row) != self.n_cols:
+                raise HofsException(
+                    "the number of row values must be equal to the number of columns"
+                )
+
+            row_dict = {
+                col_name: row[row_idx]
+                for row_idx, col_name in enumerate(self.col_names)
+            }
+        elif isinstance(row, Dict):
+            row_dict = row
+        else:
+            raise HofsException(
+                f"row must be either a dictionary or a list, but was {type(row)} instead"
             )
 
-        s = ""
-        s += (
+        cols_diff = set(row_dict.keys()).symmetric_difference(set(self._cols.keys()))
+        if len(cols_diff) != 0:
+            raise HofsException(
+                f"the row keys must be the column names, but the following keys differed: {cols_diff!r}"
+            )
+
+        for k, v in row_dict.items():
+            self._cols[k].append(v)
+
+    def __getitem__(self, i: int) -> Dict[str, str]:
+        return self.row_dict(i)
+
+    def __len__(self) -> int:
+        return self.n_rows
+
+    def __header(self, col_ljust_vals: Dict[str, int]) -> str:
+        return (
             "".join(
                 [
-                    str(col_name).ljust(ljust_values[col_name])
-                    for col_name in self.columns.keys()
+                    str(col_name).ljust(col_ljust_vals[col_name])
+                    for col_name in self.col_names
                 ]
             )
             + "\n"
         )
-        for i in range(len(self)):
-            s += (
-                "".join(
-                    [
-                        str(col).ljust(ljust_values[col_name])
-                        for col_name, col in self[i].items()
-                    ]
-                )
-                + "\n"
+
+    def __col_str(self, col_ljust_vals: Dict[str, int], row_idx: int) -> str:
+        return (
+            "".join(
+                [
+                    str(col).ljust(col_ljust_vals[col_name])
+                    for col_name, col in self[row_idx].items()
+                ]
             )
+            + "\n"
+        )
+
+    def __str__(self) -> str:
+        col_ljust_vals = {}
+        for col_name, col in self._cols.items():
+            values = self.col_by_name(col_name) + [col_name]
+            col_ljust_vals[col_name] = len(max(values, key=len)) + 1
+
+        s = self.__header(col_ljust_vals)
+        for row_idx in range(len(self)):
+            s += self.__col_str(col_ljust_vals, row_idx)
         return s
 
     def __repr__(self) -> str:
-        return json.dumps({"cols": self.columns})
+        return json.dumps({"cols": self._cols})
--- test/common/modules/test_table.py
+++ test/common/modules/test_table.py
@@ -9,8 +9,61 @@ class TestTable(TestCase):
         self.table.add_row({"Col1": "A", "Col2": "B", "Col3": "C"})
         self.table.add_row({"Col1": "D", "Col2": "E", "Col3": "F"})
 
-    def test_exception(self) -> None:
+    def test_table_zero_cols_exception(self) -> None:
+        self.assertRaises(fs.HofsException, fs.Table, [])
+
+    def test_col_name(self) -> None:
+        self.assertEqual(self.table.col_name(1), "Col2")
+
+    def test_col_names(self) -> None:
+        self.assertEqual(self.table.col_names, ["Col1", "Col2", "Col3"])
+
+    def test_col(self) -> None:
+        self.assertEqual(self.table.col(2), ["C", "F"])
+
+    def test_col_by_name(self) -> None:
+        self.assertEqual(self.table.col_by_name("Col1"), ["A", "D"])
+
+    def test_value(self) -> None:
+        self.assertEqual(self.table.value(1, 2), "F")
+
+    def test_value_by_name(self) -> None:
+        self.assertEqual(self.table.value_by_name(1, "Col3"), "F")
+
+    def test_row(self) -> None:
+        self.assertEqual(self.table.row(1), ["D", "E", "F"])
+
+    def test_row_dict(self) -> None:
+        self.assertEqual(
+            self.table.row_dict(0), {"Col1": "A", "Col2": "B", "Col3": "C"}
+        )
+
+    def test_n_rows(self) -> None:
+        self.assertEqual(self.table.n_rows, 2)
+
+    def test_n_cols(self) -> None:
+        self.assertEqual(self.table.n_cols, 3)
+
+    def test_add_row_wrong_len_exception(self) -> None:
+        self.assertRaises(fs.HofsException, self.table.add_row, ["G", "H"])
+
+    def test_add_row_wrong_type_exception(self) -> None:
+        self.assertRaises(fs.HofsException, self.table.add_row, 3)
+
+    def test_add_row_keys_diff_exception(self) -> None:
         self.assertRaises(fs.HofsException, self.table.add_row, {"Col1": "G"})
+
+    def test_add_row_list(self) -> None:
+        self.table.add_row(["G", "H", "I"])
+        self.assertEqual(self.table.row(2), ["G", "H", "I"])
+
+    def test_add_row_dict(self) -> None:
+        self.table.add_row({"Col1": "G", "Col2": "H", "Col3": "I"})
+        self.assertEqual(self.table.row(2), ["G", "H", "I"])
+
+    def test_add_row_dict_other_order(self) -> None:
+        self.table.add_row({"Col2": "H", "Col3": "I", "Col1": "G"})
+        self.assertEqual(self.table.row(2), ["G", "H", "I"])
 
     def test_getitem(self) -> None:
         self.assertEqual(self.table[1], {"Col1": "D", "Col2": "E", "Col3": "F"})
